d8175062ad7129dc97c783ce9b83964e5fd44ec1
Legg <dlegg@jpl.nasa.gov>
2018-07-18 14:58:05 -0700

    Fixed merge bug

--- src/gov/nasa/jpl/ae/event/Dependency.java
+++ src/gov/nasa/jpl/ae/event/Dependency.java
@@ -267,7 +267,6 @@ public class Dependency< T > extends HasIdImpl
     expression.satisfy(deep, seen);
     T oldValue = parameter.getValueNoPropagate();
     //if ( expression.isGrounded(deep, null) ) {
-    T oldValue = parameter.getValue( false );
       boolean applied = apply( true );
     //} else {
     //  parameter.satisfy(deep, seen);
